7e8f14ee4cdfc79e40d86f669b9a42c181429a4e
Arik096 <arikbncc@gmail.com>
2020-03-24 10:36:21 +0600

    Update 1_SQL.sql

--- SQL/1_SQL.sql
+++ SQL/1_SQL.sql
@@ -1,5 +1,11 @@
-// table name --> Customers
+// table 1 name --> Customers
 // columns --> CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country
+
+// table 2 name --> Products
+//columns --> ProductID, ProductName, SupplierID, CategoryID, Unit, Price
+
+// table 3 name --> Orders
+// columns --> OrderID,	CustomerID,	EmployeeID,	OrderDate, ShipperID
 
 
 // SELECT
@@ -63,3 +69,47 @@ SELECT MAX(Price) AS maxPrice FROM Products;
 
 
 // COUNT AVG SUM
+SELECT COUNT(ProductID) FROM Products;
+SELECT AVG(Price) FROM Products;
+SELECT SUM(Price) FROM Products;
+
+
+// LIKE
+SELECT * FROM Customers WHERE CustomerName LIKE 'a%'; --> starts with a [arik]
+SELECT * FROM Customers WHERE CustomerName LIKE '%a'; --> ends with a [puja]
+SELECT * FROM Customers WHERE CustomerName LIKE '%a%'; --> a in any position [biswas]
+
+SELECT * FROM Customers WHERE CustomerName LIKE '_r%'; --> r in the 2nd position [arik]
+SELECT * FROM Customers WHERE CustomerName LIKE '__i%'; --> i the 3rd position [arik]
+
+SELECT * FROM Customers WHERE CustomerName LIKE 'a_%'; --> a in 1st the position and the length of name is 2 character [ab]
+SELECT * FROM Customers WHERE CustomerName LIKE 'a___%'; --> a in the 1st position and the length of name is 4 character [arik]
+
+SELECT * FROM Customers WHERE CustomerName LIKE 'a%k'; --> start with a and end with k [arik]
+
+SELECT * FROM Customers WHERE CustomerName LIKE '_rik'; --> it can be start with any character but end with "rik"
+SELECT * FROM Customers WHERE CustomerName LIKE 'a_r_'; --> [arik]
+SELECT * FROM Customers WHERE CustomerName LIKE '_uj_'; --> [puja]
+
+SELECT * FROM Customers WHERE CustomerName LIKE '[a,b,c]%'; --> can start with 'a', 'b' or 'c'
+
+
+// IN --> shorthand for OR
+SELECT * FROM Customers WHERE Country IN ('UK','USA','Mexico');
+
+// NOT IN
+SELECT * FROM Customers WHERE Country NOT IN ('UK','USA','Mexico');
+
+
+// BETWEEN
+SELECT * FROM Products WHERE Price BETWEEN 10 AND 20;
+SELECT * FROM Products WHERE Price BETWEEN 10 AND 20 AND CategoryID IN (1,2,3);
+SELECT * FROM Products WHERE Price BETWEEN 10 AND 20 AND CategoryID NOT IN (1,2,3);
+SELECT * FROM Products WHERE ProductName BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni' ORDER BY ProductName; --> for text
+SELECT * FROM Orders WHERE OrderDate BETWEEN #01/07/1996# AND #31/07/1996#; --> for date
+SELECT * FROM Orders WHERE OrderDate BETWEEN '1996-07-01' AND '1996-07-31'; --> for date
+
+
+// NOT BETWEEN
+SELECT * FROM Products WHERE Price NOT BETWEEN 10 AND 20;
+SELECT * FROM Products WHERE ProductName NOT BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni' ORDER BY ProductName;
