cc3ed544977054b3f9bd8203d4f0f49e5fa19145
Arik096 <arikbncc@gmail.com>
2020-03-24 10:36:27 +0600

    Create 2_Join.sql

---
+++ SQL/2_Join.sql
@@ -0,0 +1,8 @@
+// table 1 name --> Customers
+// columns --> CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country
+
+// table 2 name --> Products
+//columns --> ProductID, ProductName, SupplierID, CategoryID, Unit, Price
+
+// table 3 name --> Orders
+// columns --> OrderID,	CustomerID,	EmployeeID,	OrderDate, ShipperID
