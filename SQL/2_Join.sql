f9dd80e9b72f91e1de7b4a9b5f8855aacb2ed676
Arik096 <arikbncc@gmail.com>
2020-03-24 11:19:11 +0600

    Update 2_Join.sql

--- SQL/2_Join.sql
+++ SQL/2_Join.sql
@@ -6,3 +6,27 @@
 
 // table 3 name --> Orders
 // columns --> OrderID,	CustomerID,	EmployeeID,	OrderDate, ShipperID
+
+// table 4 name --> Suppliers
+// columns --> SupplierID,	SupplierName,	ContactName,	Address,	City,	PostalCode,	Country
+
+
+// INNER JOIN
+SELECT Orders.OrderID, Customers.CustomerID FROM Orders INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+
+// LEFT JOIN
+SELECT Customers.CustomerID, Orders.OrderID FROM Customers LEFT JOIN Orders ON Customers.CustomerID=Orders.CustomerID;
+
+// RIGHT JOIN
+SELECT Orders.OrderID, Customers.CustomerID FROM Orders RIGHT JOIN Customers On Orders.CustomerID=Customers.CustomerID;
+
+// FULL OUTER JOIN
+SELECT Customers.CustomerID, Orders.OrderID FROM Customers FULL OUTER JOIN Orders on Customers.CustomerID=Orders.CustomerID;
+
+
+// UNION
+SELECT City FROM Customers UNION SELECT City FROM Suppliers;
+SELECT City, Country FROM Customers WHERE Country='Germany' UNION SELECT City, Country FROM Suppliers WHERE Country='Germany';
+
+// UNION ALL --> returns duplicate also
+SELECT City FROM Customers UNION ALL SELECT City FROM Suppliers;
