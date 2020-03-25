ba55837000e7c7b890e2923b6b2e0faba429cc9e
Arik096 <arikbncc@gmail.com>
2020-03-25 09:18:27 +0600

    Create 0_SQL.sql

---
+++ SQL/0_SQL.sql
@@ -0,0 +1,23 @@
+--> Northwind sample database <--
+
+
+// table 1 name --> Customers
+// columns --> CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country
+
+// table 2 name --> Products
+//columns --> ProductID, ProductName, SupplierID, CategoryID, Unit, Price
+
+// table 3 name --> Orders
+// columns --> OrderID,	CustomerID,	EmployeeID,	OrderDate, ShipperID
+
+// table 4 name --> Suppliers
+// columns --> SupplierID,	SupplierName,	ContactName,	Address,	City,	PostalCode,	Country
+
+// table 5 name --> Shippers
+// columns --> ShipperID,	ShipperName
+
+// table 6 name --> Employees
+// columns --> EmployeeID,	LastName,	FirstName,	BirthDate,	Photo,	Notes
+
+// table 7 name --> OrderDetails
+// columns --> OrderDetailID,	OrderID,	ProductID,	Quantity
