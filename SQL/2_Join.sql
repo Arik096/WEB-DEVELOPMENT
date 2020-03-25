b2cc5766b99a30f64eada7cb4c21f9b419e38aeb
Arik096 <arikbncc@gmail.com>
2020-03-25 09:18:48 +0600

    Update 2_Join.sql

--- SQL/2_Join.sql
+++ SQL/2_Join.sql
@@ -1,3 +1,6 @@
+--> Northwind sample database <--
+
+
 // table 1 name --> Customers
 // columns --> CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country
 
@@ -10,6 +13,15 @@
 // table 4 name --> Suppliers
 // columns --> SupplierID,	SupplierName,	ContactName,	Address,	City,	PostalCode,	Country
 
+// table 5 name --> Shippers
+// columns --> ShipperID,	ShipperName
+
+// table 6 name --> Employees
+// columns --> EmployeeID,	LastName,	FirstName,	BirthDate,	Photo,	Notes
+
+// table 7 name --> OrderDetails
+// columns --> OrderDetailID,	OrderID,	ProductID,	Quantity
+
 
 // INNER JOIN
 SELECT Orders.OrderID, Customers.CustomerID FROM Orders INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
@@ -18,10 +30,10 @@ SELECT Orders.OrderID, Customers.CustomerID FROM Orders INNER JOIN Customers ON 
 SELECT Customers.CustomerID, Orders.OrderID FROM Customers LEFT JOIN Orders ON Customers.CustomerID=Orders.CustomerID;
 
 // RIGHT JOIN
-SELECT Orders.OrderID, Customers.CustomerID FROM Orders RIGHT JOIN Customers On Orders.CustomerID=Customers.CustomerID;
+SELECT Orders.OrderID, Customers.CustomerID FROM Orders RIGHT JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
 
 // FULL OUTER JOIN
-SELECT Customers.CustomerID, Orders.OrderID FROM Customers FULL OUTER JOIN Orders on Customers.CustomerID=Orders.CustomerID;
+SELECT Customers.CustomerID, Orders.OrderID FROM Customers FULL OUTER JOIN Orders ON Customers.CustomerID=Orders.CustomerID;
 
 
 // UNION
@@ -30,3 +42,49 @@ SELECT City, Country FROM Customers WHERE Country='Germany' UNION SELECT City, C
 
 // UNION ALL --> returns duplicate also
 SELECT City FROM Customers UNION ALL SELECT City FROM Suppliers;
+
+
+// GROUP BY
+SELECT COUNT(CustomerID), Country FROM Customers GROUP BY Country;
+SELECT COUNT(CustomerID), Country FROM Customers GROUP BY Country ORDER BY COUNT(CustomerID) DESC;
+SELECT Shippers.ShipperName, COUNT(Orders.OrderID) FROM Orders LEFT JOIN Shippers ON Orders.ShipperID=Shippers.ShipperID GROUP bY ShipperName;
+
+
+// HAVING --> when WHERE cluse dont work
+SELECT COUNT(CustomerID), Country FROM Customers GROUP BY Country HAVING COUNT(CustomerID)>5;
+SELECT Employees.FirstName, COUNT(Orders.OrderID) FROM Orders INNER JOIN Employees ON Orders.EmployeeID=Employees.EmployeeID HAVING COUNT(Orders.OrderID)>10;
+
+
+// SELECT INTO --> copy some columns into a new table
+SELECT CustomerID, CustomerName INTO New_Customers FROM Customers WHERE Country='USA';
+
+
+// INSERT INTO SELECT --> copy columns into a existing table
+INSERT INTO Customers_2 SELECT * FROM Customers WHERE Country='USA';
+
+// CASE
+SELECT OrderID, Quantity
+CASE
+    WHEN Quantity>30 THEN 'Quantity is more then 30'
+    WHEN Quantity=30 THEN 'Quantity is equal to 30'
+    ELSE 'Quantity is less then 30'
+END
+FROM OrderDetails;
+
+SELECT CustomerName, City, Country FROM Customers ORDER BY
+(
+  CASE
+      WHEN City IS NULL THEN Country
+      ELSE City
+  END
+);
+
+
+// PROCEDURE
+CREATE PROCEDURE SelectAllCustomers
+AS
+SELECT * FROM Customers
+GO;
+
+// EXEC PROCEDURE
+EXEC SelectAllCustomers;
