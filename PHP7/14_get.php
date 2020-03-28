912e1c295479fa2a8648a5a2ec8769bea4c59cc9
Arik096 <arikbncc@gmail.com>
2020-03-28 09:46:33 +0600

    Create 14_get.php

---
+++ PHP7/14_get.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="css/14_style.css">
+    <title>form handling</title>
+  </head>
+  <body>
+    <form id="f1" action="14_output_page.php" method="get">
+      <label id="l1">Form</label>
+      <br>
+      <label id="l2">Name:</label>
+      <input id="i1" type="text" name="name">
+      <br>
+      <label id="l3">Email:</label>
+      <input id="i2" type="email" name="email">
+      <br>
+      <input type="submit">
+    </form>
+  </body>
+</html>
