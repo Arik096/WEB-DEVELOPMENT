a9687ad9a57e1c5a9eb142508510a7c9ed6b5a79
Arik096 <arikbncc@gmail.com>
2020-03-28 09:46:31 +0600

    Create 13_post.php

---
+++ PHP7/13_post.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="css/13_style.css">
+    <title>form handling</title>
+  </head>
+  <body>
+    <form id="f1" action="13_output_page.php" method="post">
+      <label id="l1">From</label>
+      <br>
+      <label for="">Name:</label>
+      <input id="i1" type="text" name="name">
+      <br>
+      <label for="">Email:</label>
+      <input id="i2" type="text" name="email">
+      <br>
+      <input type="submit">
+    </form>
+  </body>
+</html>
