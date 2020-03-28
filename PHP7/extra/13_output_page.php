74c832c347a54a699eac85514d63ab54c2aa0844
Arik096 <arikbncc@gmail.com>
2020-03-28 11:16:12 +0600

    Create 13_output_page.php

---
+++ PHP7/extra/13_output_page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> form output</title>
+  </head>
+  <body>
+    My name is
+    <?php echo $_POST["name"]; ?>
+    <br>
+    Email:
+    <?php echo $_POST["email"]; ?>
+  </body>
+</html>
