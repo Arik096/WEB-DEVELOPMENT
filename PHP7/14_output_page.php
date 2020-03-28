f7c101b03ef088bf6d01e11f59b176646133d983
Arik096 <arikbncc@gmail.com>
2020-03-28 09:46:35 +0600

    Create 14_output_page.php

---
+++ PHP7/14_output_page.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>form handling</title>
+  </head>
+  <body>
+    welcome <?php echo $_GET["name"]; ?>
+    <br>
+    Email: <?php echo $_GET["email"]; ?>
+  </body>
+</html>
