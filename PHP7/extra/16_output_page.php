b25586c316f3fdda6a0ff4cbe1a428f4a6b61616
Arik096 <arikbncc@gmail.com>
2020-03-29 11:16:20 +0600

    Create 16_output_page.php

---
+++ PHP7/extra/16_output_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <title>form Validation</title>
+  </head>
+  <body>
+    <?php echo $_POST["name"]; ?>
+    <br>
+    <?php echo $_POST["email"]; ?>
+    <br>
+    <?php echo $_POST["website"]; ?>
+    <br>
+    <?php echo $_POST["comment"]; ?>
+    <br>
+    <?php echo $_POST["gender"]; ?>
+    <br>
+  </body>
+</html>
