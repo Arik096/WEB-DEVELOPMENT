4974973168a7baf96b0ac0cb46848fc9ccbb1554
Arik096 <arikbncc@gmail.com>
2020-03-29 10:42:49 +0600

    Update 15_form_validation.php

--- PHP7/15_form_validation.php
+++ PHP7/15_form_validation.php
@@ -4,17 +4,45 @@
     <title>form Validation</title>
   </head>
   <body>
-    <form class="" action="extra/15_output_page.php" method="post">
+
+    <?php
+    $name = "";
+    $email = "";
+    $website = "";
+    $comment = "";
+    $gender = "";
+
+    if($_SERVER["REQUEST_METHOD"]=="POST")
+    {
+      $name = test_input($_POST["name"]);
+      $email = test_input($_POST["email"]);
+      $websie = test_input($_POST["website"]);
+      $comment = test_input($_POST["comment"]);
+      $gender = test_input($_POST["gender"]);
+    }
+
+    function test_input($data)
+    {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+
+      return $data;
+    }
+
+     ?>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
       <h1>PHP From Validation</h1>
       <br>
-      <p style="color:red">*required field</p>
+      <p style="color:red"><sup>*</sup>required field</p>
       <br>
       <fieldset style="line-height:30px;">
         <label>Name:</label>
-        <input type="text" name="name">
+        <input type="text" name="name"><sup style="color:red">*</sup>
         <br>
         <label>Email:</label>
-        <input type="email" name="email">
+        <input type="email" name="email"><sup style="color:red">*</sup>
         <br>
         <label>Website:</label>
         <input type="text" name="website">
@@ -26,7 +54,7 @@
         <input type="radio" name="gender" value="Male">Male
         <input type="radio" name="gender" value="Female">Female
         <input type="radio" name="gender" value="Other
-        ">Other
+        ">Other<sup style="color:red">*</sup>
         <br>
         <input type="submit">
       </fieldset>
