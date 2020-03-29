16f529c2255909bd2a01ac860c047a1fc8fef595
Arik096 <arikbncc@gmail.com>
2020-03-29 11:16:18 +0600

    Create 16_form_required.php

---
+++ PHP7/16_form_required.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>form Validation</title>
+  </head>
+  <body>
+
+    <?php
+    $name = "";
+    $email = "";
+    $website = "";
+    $comment = "";
+    $gender = "";
+
+    $nameError = "";
+    $emailError = "";
+    $genderError = "";
+
+    if($_SERVER["REQUEST_METHOD"]=="POST")
+    {
+      if(empty($_POST["name"]))
+        $nameError = "Name is required";
+      else
+        $name = test_input($_POST["name"]);
+
+      if(empty($_POST["email"]))
+        $emailError = "Email is required";
+      else
+        $email = test_input($_POST["email"]);
+
+      $website = test_input($_POST["website"]);
+
+      $comment = test_input($_POST["comment"]);
+
+      if(empty($_POST["gender"]))
+        $genderError = "Gender is required";
+      else
+        $gender = test_input($_POST["gender"]);
+    }
+
+
+    function test_input($data)
+    {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+
+      return $data;
+    }
+     ?>
+
+
+
+    <form action="extra/16_output_page.php" method="post">
+      <h1>PHP From Validation</h1>
+      <br>
+      <p style="color:red"><sup>*</sup>required field</p>
+      <br>
+      <fieldset style="line-height:30px;">
+        <label>Name:</label>
+        <input type="text" name="name"><sup style="color:red">*</sup><span><?php echo $nameError; ?></span>
+        <br>
+        <label>Email:</label>
+        <input type="email" name="email"><sup style="color:red">*</sup><span><?php echo $emailError; ?></span>
+        <br>
+        <label>Website:</label>
+        <input type="text" name="website">
+        <br>
+        <label>Comments:</label>
+        <textarea name="comment" rows="8" cols="30"></textarea>
+        <br>
+        <label>Gender:</label>
+        <input type="radio" name="gender" value="Male">Male
+        <input type="radio" name="gender" value="Female">Female
+        <input type="radio" name="gender" value="Other
+        ">Other<sup style="color:red">*</sup><span><?php echo $genderError; ?></span>
+        <br>
+        <input type="submit">
+      </fieldset>
+    </form>
+  </body>
+</html>
