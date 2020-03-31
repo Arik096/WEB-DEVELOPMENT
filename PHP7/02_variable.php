72b36294955c47fc59c44ee6e09d838632f40094
Arik096 <arikbncc@gmail.com>
2020-03-31 11:27:21 +0600

    Create 02_variable.php

---
+++ PHP7/02_variable.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>variable</title>
+  </head>
+  <body>
+    <?php
+    $a = 10;
+    $b = 20;
+    $c = $a+$b;
+    echo "$c <br>";
+    echo $a+$c;
+
+    $txt="arik";
+    $ar=5;
+    $pj=5.5;
+    echo "<br>".$txt."<br>".$txt;
+    echo "<br>This ", "string ", "was ", "made ", "with multiple parameters.";
+     ?>
+  </body>
+</html>
