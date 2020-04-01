735291fdc903cf7498816fd1442761c4ac325e9f
Arik096 <arikbncc@gmail.com>
2020-04-01 23:04:10 +0600

    Create 30_arnob_test.php

---
+++ PHP7/30_arnob_test.php
@@ -0,0 +1,20 @@
+<?php
+$credit = array("CSE-101"=>3, "EEE-163"=>3, "MATH-141"=>3, "HUM-415"=>2);
+$gpa = array("CSE-101"=>3, "EEE-163"=>3.25, "MATH-141"=>4, "HUM-415"=>4);
+$total_gpa_sum = 0;
+$total_credit = 0;
+$cgpa = 0;
+foreach ($credit as $key1 => $value1)
+{
+  foreach ($gpa as $key2 => $value2)
+  {
+    if($key1 == $key2)
+    {
+      $total_credit = $total_credit + $value1;
+      $total_gpa_sum = $total_gpa_sum + ($value1 * $value2);
+    }
+  }
+}
+$cgpa = $total_gpa_sum / $total_credit;
+echo $cgpa;
+ ?>
