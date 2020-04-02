03ce66bd5f25290a61f6e30893a24ed7f8d4854a
Arik096 <arikbncc@gmail.com>
2020-04-02 11:54:29 +0600

    Create 38_MySQLi_select.php

---
+++ PHP7/38_MySQLi_select.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "webDB";
+
+$con = mysqli_connect($servername, $username, $password,$dbname);
+
+if(!$con)
+{
+  die("Error: ".mysqli_connect_error());
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($con,$sql);
+
+if(mysqli_num_rows($result)>0)
+{
+  while($row=mysqli_fetch_assoc($result)){
+    echo "ID: ".$row["id"]." - "."Last Name: ".$row["lastname"]."<br>";
+  }
+}
+else {
+  echo "0 result";
+}
+ ?>
